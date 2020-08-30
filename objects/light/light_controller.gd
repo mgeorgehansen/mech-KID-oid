1847e11f7b5ec5be76acbef1ddd7b1d328dd6485
M. George Hansen <mgeorge@technopolitica.com>
2020-08-30 15:04:13 -0700

    feat: switches that control child components

--- objects/light/light_controller.gd
+++ objects/light/light_controller.gd
@@ -10,3 +10,9 @@ func turn_off() -> void:
 func turn_on() -> void:
 	enabled = true
 	light_emitter.turn_on()
+
+func receive_power(powered: bool) -> void:
+	if powered:
+		turn_on()
+	else:
+		turn_off()
---
+++ objects/switches/two-way-switch.gd
@@ -0,0 +1,47 @@
+tool
+class_name TwoWaySwitch
+extends AnimatedSprite
+
+signal switch_flipped(active_lane)
+
+var connected_components = {}
+
+export(int, 0, 1) var _active_lane: int = 0 setget _set_active_lane
+
+func _ready() -> void:
+	var lane_idx = 0;
+	for child in get_children():
+		if !child.has_method("receive_power"):
+			continue
+		assert(lane_idx <= 1, "More than two components connected to TwoWaySwitch")
+		connected_components[lane_idx] = child
+		lane_idx += 1
+	connect("switch_flipped", self, "_on_switch_flipped")
+	_on_switch_flipped(_active_lane)
+
+
+func _set_active_lane(active_lane: int) -> void:
+	_active_lane = active_lane
+	_on_switch_flipped(active_lane)
+
+
+func _on_switch_flipped(active_lane: int) -> void:
+	if active_lane == 0:
+		play("01-active")
+	else:
+		play("02-active")
+	for lane in connected_components.keys():
+		connected_components[lane].receive_power(lane == active_lane)
+
+
+func toggle() -> void:
+	if _active_lane == 1:
+		_active_lane = 0
+	else:
+		_active_lane = 1
+	emit_signal("switch_flipped", _active_lane)
+
+
+func _on_input_event(_viewport: Object, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_pressed("mouse_press"):
+		toggle()
